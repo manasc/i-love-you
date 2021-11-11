e686e1ecd8e9a2cbaf00c696b53fd26b40293632
Manas Chimpidi <manaschimpidi@gmail.com>
2021-11-10 23:47:48 -0600

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
+
 import daniela from "./images/daniela.png";
 import manas from "./images/manas.png";
 import heart from "./images/heart.png";
@@ -7,7 +9,7 @@ import { Ping } from "./components/Ping";
 import {
     BrowserRouter as Router,
     Route,
-    Switch,
+    Routes,
     useParams,
 } from "react-router-dom";
 
@@ -36,17 +38,52 @@ const colorsArr = [
     "rose",
 ];
 
+const commonMessages = [
+    "Hello hello",
+    "Hola, mi amorcito lindo",
+    "Hola, mi amor",
+    "Te quiero mucho, mi amor",
+    "Te amo mucho",
+    "Te amo tanto",
+];
+
+const manasMessages = [
+    "Hola, mi hermosa",
+    "Baby girl, te amo.",
+    "Dame la cosita",
+    ...commonMessages,
+];
+
+const daniMessages = ["Mi amooooor", "My cuuuutie", ...commonMessages];
+
+const getRandomInt = () => {
+    let max;
+
+    if (manasMessages.length >= daniMessages.length) {
+        max = manasMessages.length - 1;
+    } else {
+        max = daniMessages.length - 1;
+    }
+
+    return Math.floor(Math.random() * max);
+};
+
 // git remote add origin https://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@github.com/manasc/dnm.git
 
-const getBackgroundColor = (paramColor = "gray", shade = 900) => {
-    switch (paramColor) {
-    case "transparent":
-    case "white":
-    case "black":
+const getBackgroundColor = (paramColor, shade) => {
+    if (
+        paramColor === "white" ||
+        paramColor === "black" ||
+        paramColor === "transparents"
+    ) {
         return `bg-${paramColor}`;
-    default:
+    }
+
+    if (colorsArr.includes(paramColor) && shade) {
         return `bg-${paramColor}-${shade}`;
     }
+
+    return "bg-pink-200";
 };
 
 const Page = ({ embed }) => {
@@ -54,20 +91,48 @@ const Page = ({ embed }) => {
     const bgColor = getBackgroundColor(color, shade);
     const boxPadding = embed ? "" : "pb-10";
 
+    const [daniMessage, setDaniMessage] = React.useState(
+        daniMessages[getRandomInt()]
+    );
+    const [manasMessage, setManasMessage] = React.useState(
+        manasMessages[getRandomInt()]
+    );
+
+    const changeManasMessage = () => {
+        setManasMessage(manasMessages[getRandomInt()]);
+
+        if (manasMessage === "Dame la cosita") {
+            setDaniMessage("Ya te la di!");
+        }
+    };
+    const changeDaniMessage = () => {
+        setDaniMessage(daniMessages[getRandomInt()]);
+
+        if (daniMessage === "Dame la cosita") {
+            setManasMessage("Ya te la di!");
+        }
+    };
+
     return (
         <div
             className={`h-screen w-screen flex flex-col items-center justify-center ${boxPadding} ${bgColor}`}
         >
             <div className="relative mb-1 flex">
-                <div className="px-1 z-40">
+                <div
+                    className="p-5 cursor-pointer"
+                    data-tip={daniMessage}
+                    onMouseLeave={changeDaniMessage}
+                >
                     <img
                         alt="daniela"
                         src={daniela}
-                        width={40}
-                        height={"auto"}
+                        style={{
+                            height: 35,
+                            width: "auto",
+                        }}
                     />
                 </div>
-                <div className="px-1 relative flex items-center justify-center bg z-0">
+                <div className="px-2 relative flex items-center justify-center bg z-0">
                     <Ping size={9} opacity={75} />
                     <Ping size={7} opacity={50} />
                     <Ping size={5} opacity={50} />
@@ -80,27 +145,51 @@ const Page = ({ embed }) => {
                         />
                     </div>
                 </div>
-                <div className="px-1 z-40">
-                    <img alt="manas" src={manas} width={40} height={"auto"} />
+                <div
+                    className="p-5 cursor-pointer"
+                    data-tip={manasMessage}
+                    onMouseLeave={changeManasMessage}
+                >
+                    <img
+                        alt="manas"
+                        src={manas}
+                        style={{
+                            height: 35,
+                            width: "auto",
+                        }}
+                    />
                 </div>
             </div>
         </div>
     );
 };
 
-const App = () => {
-    return (
+const App = () => (
+    <div>
         <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Page />
+            <Routes>
+                <Route exact path="/" element={<Page />}>
+                    <Route path=":color" element={<Page />} />
+                    <Route path=":color/:shade" element={<Page />} />
                 </Route>
-                <Route path="/embed/:color?/:shade?">
-                    <Page embed={true} />
+
+                <Route path="/embed" element={<Page embed={true} />}>
+                    <Route path=":color" element={<Page embed={true} />} />
+                    <Route
+                        path=":color/:shade"
+                        element={<Page embed={true} />}
+                    />
                 </Route>
-            </Switch>
+            </Routes>
         </Router>
-    );
-};
+        <ReactTooltip
+            type="light"
+            border={true}
+            padding="8px 16px"
+            offset={{ top: 20 }}
+            // delayHide={3000}
+        />
+    </div>
+);
 
 export default App;
--- src/components/Ping.jsx
+++ src/components/Ping.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
-export const Ping = ({size = 9, color = 'red', opacity = 100}) => {
+export const Ping = ({ size = 9, color = "red", opacity = 100 }) => {
     return (
-        <div className={`animate-ping-heart absolute inline-flex h-${size} w-${size} rounded-full bg-${color}-500 opacity-${opacity}`} />
-    )
-}
+        <div
+            className={`animate-ping-heart absolute inline-flex h-${size} w-${size} rounded-full bg-${color}-500 opacity-${opacity}`}
+        />
+    );
+};
