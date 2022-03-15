304588325bbfed17e4fc5eb8c797f95e372724fc
Manas Chimpidi <manaschimpidi@gmail.com>
2022-03-14 23:02:29 -0500

    added meta tags

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Ping from "components/Ping";
 import Image from "next/image";
 import imageLoader from "utils/imageLoader";
+import Head from "next/head";
 import { useContextProvider } from "providers/GlobalContextProvider";
 import ReactTooltip from "react-tooltip";
 
@@ -17,6 +18,42 @@ export default function Home() {
 
 	return (
 		<>
+			<Head>
+				<meta charset="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1"
+				/>
+				<meta name="description" content="D&M" />
+				<link rel="icon" href="favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="152x152"
+					href="apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="favicon-16x16.png"
+				/>
+				<link rel="manifest" href="site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="safari-pinned-tab.svg"
+					color="#5bbad5"
+				/>
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff" />
+				<link rel="manifest" href="manifest.json" />
+				<title>{"D&M"}</title>
+			</Head>
 			<div
 				className={`h-screen w-screen flex flex-col items-center justify-center ${boxPadding} ${bgColor}`}
 			>
@@ -64,7 +101,7 @@ export default function Home() {
 				</div>
 				{!Boolean(embed) && (
 					<div className="font-serif pt-4 opacity-75 text-sm text-center">
-						Hazme un clic y te daré un poquito amor.
+						{"Hazme un clic y te daré un poquito amor."}
 					</div>
 				)}
 			</div>
