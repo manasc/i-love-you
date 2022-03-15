08a1042daafd364acf8d76602e576228473a8acc
Manas Chimpidi <manaschimpidi@gmail.com>
2022-03-14 22:52:57 -0500

    Updates

---
+++ _next/static/58WK97WI14qWP1KSrhEd0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/366-b87a8db887f4b3e4.js","static/chunks/pages/index-88b8f66d30f6f78b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
