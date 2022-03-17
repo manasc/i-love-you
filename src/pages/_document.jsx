1598b376e6c4b46679aa355314d10bff6a63d557
Manas Chimpidi <manaschimpidi@gmail.com>
2022-03-16 21:16:29 -0500

    added hotjar

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -20,6 +20,21 @@ export default function Document() {
 						`,
 					}}
 				/>
+				{/* <!-- Hotjar Tracking Code for https://dnm.manasc.com --> */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(h,o,t,j,a,r){
+								h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+								h._hjSettings={hjid:2879931,hjsv:6};
+								a=o.getElementsByTagName('head')[0];
+								r=o.createElement('script');r.async=1;
+								r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+								a.appendChild(r);
+							})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+						`,
+					}}
+				/>
 			</Head>
 			<body>
 				<Main />
